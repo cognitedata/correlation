92fffb465a991cb82aaf28ecd408fba088b98205
marcelroed <marcelroed@gmail.com>
2019-07-04 13:32:16 +0200

    Proper name in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 version = re.search('^__version__\s*=\s*"(.*)"', open("cognite/package_template/__init__.py").read(), re.M).group(1)
 
 setup(
-    name="cognite-package_template",
+    name="cognite-correlation",
     version=version,
     packages=["cognite." + p for p in find_packages(where="cognite")],
     install_requires=['pandas', 'numpy'],
